2aec60f0cc60f2552a212219b7be729bac3d0878
bensteinberg <bsteinberg@law.harvard.edu>
2023-01-26 14:47:02 -0500

    Don't expire cache keys

--- config/settings.py
+++ config/settings.py
@@ -149,7 +149,8 @@ CACHES = {
         "LOCATION": env("REDIS_URL", default="redis://127.0.0.1:6379/0"),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
+        },
+        "TIMEOUT": None
     }
 }
 
