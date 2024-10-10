560b1b1c0b5a86089581054d3eb9567664477188
bensteinberg <bsteinberg@law.harvard.edu>
2024-10-10 14:55:36 -0400

    Update to Django 5

--- config/asgi.py
+++ config/asgi.py
@@ -4,9 +4,20 @@ defined in the ASGI_APPLICATION setting.
 """
 
 import os
-import django
-from channels.routing import get_default_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+
+from main import routing
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-django.setup()
-application = get_default_application()
+
+application = ProtocolTypeRouter({
+  "http": get_asgi_application(),
+  "websocket": AuthMiddlewareStack(
+        URLRouter(
+            routing.websocket_urlpatterns
+        )
+    ),
+})
--- main/routing.py
+++ main/routing.py
@@ -1,7 +1,8 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import consumers
 
+
 websocket_urlpatterns = [
-    url(r'^ws/$', consumers.Consumer),
+    re_path(r'^ws/$', consumers.Consumer.as_asgi()),
 ]
